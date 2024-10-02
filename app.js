816663a95fa6b509c0075e24e0022ce0a137d6e1
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2024-10-02 20:16:31 +0200

    better sessions

--- app.js
+++ app.js
@@ -1,37 +1,45 @@
 const express = require('express');
+const session = require('express-session')
 const path = require('path');
+const MongoDBStore = require('connect-mongo')
+
 const MongoConnector = require('./MongoConnector').MongoConnector;
 
+connectionString = process.env.DATABASE_URL || "mongodb://localhost:27017";
+const port = 8080;
+
+//create express app
 const app = express();
 app.use(express.json());
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = 8080;
-connectionString = process.env.DATABASE_URL || "mongodb://localhost:27017";
-
-const session = require('express-session')
-const MongoDBStore = require('connect-mongodb-session')(session);
-const store = new MongoDBStore({
-    uri: connectionString,
-    collection: 'sessions'
-});
+//use db to store session
 app.use(session({
-    secret: 'transmitter-secret',
+    secret: 'transmitter secret',
+    httpOnly: true,
+    secure: true,
+    maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
-    store: store
+    store: MongoDBStore.create({
+        mongoUrl: connectionString,
+        dbName: 'transmitter',
+        collectionName: 'sessions'
+    })
 }));
 
+//connect to db
 const db = new MongoConnector("transmitter", connectionString);
 
+//routes
 app.get('/', (req, res) => {
     req.session.views = (req.session.views || 0) + 1;
     res.send('Hello World!');
 });
 
+//start server
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
