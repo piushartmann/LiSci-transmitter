97b86eadf3a3b35972dc4156e495ec87847cc4b5
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2024-10-02 19:28:57 +0200

    better database connections

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = express();
 const port = 8080;
+const MongoConnector = require('./MongoConnector').MongoConnector;
+
+connectionString = process.env.DATABASE_URL || "mongodb://localhost:27017";
+
+const db = new MongoConnector("transmitter", connectionString);
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
--- test.js
+++ test.js
@@ -2,27 +2,27 @@ const MongoConnector = require('./MongoConnector').MongoConnector;
 
 connectionString = process.env.DATABASE_URL || "mongodb://localhost:27017";
 
-const connector = new MongoConnector(connectionString);
-connector.dropDatabase().then(() => { console.log("Database dropped"); });
+const db = new MongoConnector("transmitter-test", connectionString);
+db.dropDatabase().then(() => { console.log("Database dropped"); });
 
 async function test() {
 
-    userID = (await connector.createUser("testuser", "password", "admin"))._id;
+    userID = (await db.createUser("testuser", "password", "admin"))._id;
     console.log("Created user with ID: " + userID);
 
-    postID = (await connector.createPost(userID, "title", "content", "type", "classmatesonly"))._id;
+    postID = (await db.createPost(userID, "title", "content", "type", "classmatesonly"))._id;
     console.log("Created post with ID: " + postID);
 
-    await connector.likePost(postID, userID).then(() => { console.log("Post liked"); });
-    await connector.commentPost(postID, userID, "content", "classmatesonly").then(() => { console.log("Post commented"); });
+    await db.likePost(postID, userID).then(() => { console.log("Post liked"); });
+    await db.commentPost(postID, userID, "content", "classmatesonly").then(() => { console.log("Post commented"); });
 
-    const post = await connector.getPost(postID);
+    const post = await db.getPost(postID);
     console.log(`Post from ${post.userID.username} with ${post.comments.length} comments and ${post.likes.length} likes`);
 
-    const comments = await connector.loadPostComments(postID);
+    const comments = await db.loadPostComments(postID);
     console.log(`Comment0: ${comments[0].userID.username} said ${comments[0].content} this comment has ${comments[0].likes.length} likes`);
 
-    const likes = await connector.loadLikesForPost(postID);
+    const likes = await db.loadLikesForPost(postID);
     console.log(`Like0: ${likes[0].userID.username}`);
 
 }
