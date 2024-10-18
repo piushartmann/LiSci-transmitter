a9d7d7c9126ee5dbfa87a23b92ab66cd2f6e1940
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2024-10-18 17:02:45 +0200

    add tic tac toe visuals

---
+++ public/js/games/tic-tac-toe.js
@@ -0,0 +1,41 @@
+let yourTurn = true;
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const squares = Array.from(document.getElementsByClassName('square'));
+    squares.forEach(square => {
+        square.addEventListener('click', () => {
+            if (!yourTurn) {
+                return;
+            }
+            if (square.textContent === '') {
+                square.textContent = 'X';
+                
+                yourTurn = false;
+
+                fetch('/games/tic-tac-toe/move', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        board: Array.from(squares).map(square => square.textContent),
+                    })
+                }).then(async (res) => {
+                    if (res.ok) {
+                        updateBoard(await res.json());
+                    }
+                })
+            }
+
+        });
+    });
+});
+
+
+function updateBoard(board) {
+    const square = board.square;
+    console.log(board);
+    const squares = Array.from(document.getElementsByClassName('square'));
+    squares[square].textContent = 'O';
+    yourTurn = true;
+}
--- routes/games/tic-tac-toe.js
+++ routes/games/tic-tac-toe.js
@@ -4,8 +4,28 @@ const router = Router();
 
 module.exports = (db) => {
 
-    router.get('/', (req, res) => {
-        res.send("here will be tic tac toe");
+    router.get('/', async (req, res) => {
+
+        const permissions = await db.getUserPermissions(req.session.userID);
+
+        return res.render('games/tic-tac-toe', {
+            loggedIn: typeof req.session.username != "undefined", username: req.session.username, usertype: permissions, profilePic: await db.getPreference(req.session.userID, 'profilePic'),
+        });
+    });
+
+    router.post('/move', async (req, res) => {
+
+        const { board } = req.body;
+
+        let attempts = 0;
+        newPosition = Math.floor(Math.random() * 9);
+        while (board[newPosition] !== "" && attempts < 8) {
+            newPosition = Math.floor(Math.random() * 9);
+            attempts++;
+        }
+
+        return res.status(200).send(JSON.stringify({ square: newPosition }));
+
     });
 
     return router;
