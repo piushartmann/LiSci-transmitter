771bbc03b1c2b52c5c32c6e190acb80ebef3f26c
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2024-11-13 01:12:27 +0100

    fix: build path

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-find ./public -type f -name '*.js' -exec sh -c 'terser "$1" -o "$1" --compress --mangle --source-map "base=\"./public\",filename=\"$1.map\",url=\"${1#./public}.map\""' sh {} \;
+find ./public -type f -name '*.js' -exec sh -c 'terser "$1" -o "$1" --compress --mangle --source-map "base=\"./public\",filename=\"${1#./public}.map\",url=\"${1#./public}.map\""' sh {} \;
