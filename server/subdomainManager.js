39c025f93e20090b7daa1939173a8428e3129d73
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2024-11-26 19:07:04 +0100

    fix: router not initlized correctly

--- server/subdomainManager.js
+++ server/subdomainManager.js
@@ -1,27 +1,21 @@
 const path = require('path');
 const fs = require('fs');
 
-const subdomains = fs.readdirSync( path.join(__dirname, "subdomains")).filter(file => fs.statSync(path.join( path.join(__dirname, "subdomains"), file)).isDirectory());
+const subdomains = fs.readdirSync(path.join(__dirname, "subdomains"))
+    .filter(file => fs.statSync(path.join(path.join(__dirname, "subdomains"), file)).isDirectory());
 
 function subdomainMiddleware(req, res, next) {
     if (req.subdomains.length > 0) {
         const subdomain = req.subdomains[0];
         console.log(subdomain);
         if (subdomains.includes(subdomain)) {
-            const router = require(path.join(__dirname, "subdomains", subdomain, "router.js"));
-            console.log("Loaded subdomain router");
-            if (typeof router === 'function') {
-                router(req, res, next);
-            } else if (typeof router.handle === 'function') {
-                router.handle(req, res, next);
-            } else {
-                next(new Error('Router is not a function'));
-            }
+            const routerModule = require(path.join(__dirname, "subdomains", subdomain, "router.js"));
+            const router = routerModule();
+            return router(req, res, next);
         } else {
             next();
         }
-    }
-    else{
+    } else {
         next();
     }
 }
