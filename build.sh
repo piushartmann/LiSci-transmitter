17daa208d9b0a805969ddbbfc015fa6407b926fb
piushartmann <116584229+rtx-3070@users.noreply.github.com>
2025-02-01 20:20:54 +0100

    minify all .js files in /public folders

--- build.sh
+++ build.sh
@@ -1,15 +1,13 @@
-find ./public -type f -name '*.js' -exec sh -c '
+find . -type f -path '*/public/*.js' -exec sh -c '
   for file; do
-    base_path="${file#./public/}"
+    base_path="${file#./}"
     relative_path="${base_path#*/}"
-    foldername="${base_path%/*}"
+    foldername="$(dirname "$base_path")"
     output="${file%.js}.min.js"
     map_url="${relative_path%.js}.min.js.map"
     echo "Minifying $relative_path"
-    terser "$file" -o "$output" --compress --mangle --source-map "base=\"./public/$foldername\",filename=\"$map_url\",url=\"$map_url\""
+    terser "$file" -o "$output" --compress --mangle --source-map "base=\"$foldername\",filename=\"$map_url\",url=\"$map_url\""
   done
 ' sh {} +
 
 echo "Minification complete"
-
-node "translate_language_file.js"
